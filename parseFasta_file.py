f1095c54fd709d19a61b33acb51e5315b9279357
Sonam Wangmo <swangmo@uark.edu>
2019-04-10 10:42:25 -0500

    Parsing Fasta file

---
+++ parseFasta_file.py
@@ -0,0 +1,37 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "from Bio import SeqIO\n",
+    "for record in SeqIO.parse('watermelon.fsa', 'fasta'):\n",
+    "    print(record.id)\n",
+    "    print(repr(record.seq))"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 2",
+   "language": "python",
+   "name": "python2"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 2
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython2",
+   "version": "2.7.15"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
